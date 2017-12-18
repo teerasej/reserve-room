4a16336926847ab21a3657febc80bcc1a884c4b9
Teerasej <teerasej@gmail.com>
2017-12-19 00:18:19 +0700

    Remove unused code

--- src/public/routes/report.php
+++ src/public/routes/report.php
@@ -6,11 +6,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 $app->get('/report/month', function (Request $request, Response $response) {
     $db = $this->db;
 
-    // foreach($db->query("SELECT * FROM Room") as $row){
-    //     $this->logger->addInfo($row['id'] . ' ' . $row['name']);
-    // }
-
-    $statement = $db->prepare("SELECT * FROM Room");
+    $statement = $db->prepare("SELECT * FROM Report");
     $statement->execute();
     $results = $statement->fetchAll();
     echo json_encode($results);
